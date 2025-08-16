bf8c6b29a1e4695b9f35f8936e1206c7b289a88e
Naveen Thangamalayan <naveen.thangamalayan@johnlewis.co.uk>
2025-08-16 14:24:07 +0530

    task2

---
+++ task2.py
@@ -0,0 +1,33 @@
+import requests
+
+OLLAMA_API_URL = "http://localhost:11434/api/chat"
+OLLAMA_MODEL = "qwen2.5:7b"
+
+conversation_history = []
+
+
+def call_llm_message(prompt_messages):
+    response = requests.post(OLLAMA_API_URL,
+                             json={"model": OLLAMA_MODEL, "messages": prompt_messages, "stream": False,
+                                   "format": "json"},
+                             timeout=120)  # Increased timeout for local LLM
+    response.raise_for_status()  # Raise an exception for HTTP errors
+    print(response.json())
+    llm_output = response.json()['message']['content']
+    return llm_output
+    # return llm_response(llm_output)
+
+
+def run_agent_loop():
+    while True:
+        user_input = input("You:").strip()
+        if user_input == "quit":
+            break
+        elif not user_input:
+            continue
+        conversation_history.append(({"role": "user", "content": user_input}))
+        result = call_llm_message(conversation_history)
+        print(result)
+
+
+run_agent_loop()
